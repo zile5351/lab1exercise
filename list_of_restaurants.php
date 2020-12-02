32d3970d423b5e066f7ab118783670c8a11e14aa
zile5351 <60975884+zile5351@users.noreply.github.com>
2020-12-02 17:41:56 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -19,6 +19,37 @@
 <th>Menara Standard Chartered, TPM</th>
 <th>03-22441234</th>
 </tr>
+ 
+  // establish connection with database
+  <?php
+$serverName = "simplewebtp044739.database.windows.net";
+$connectionOptions = array(
+"Database" => "simpleweb",
+"Uid" => "simpleweb",
+"PWD" => "Simple@web");
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if (!$conn)
+{
+die("Error connection: ".sqlsrv_errors());
+}
+$tsql= "SELECT * FROM [dbo].[restaurant]";
+$getResults= sqlsrv_query($conn, $tsql);
+if ($getResults == FALSE)
+{
+die(sqlsrv_errors());
+}
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+echo "<tr>";
+echo "<td>". $row['restaurant_id'] . "</td>";
+echo "<td>". $row['restaurant_name'] ."</td>";
+echo "<td>". $row['restaurant_address'] . "</td>";
+echo "<td>". $row['restaurant_phone'] . "</td>";
+echo "</tr>";
+}
+sqlsrv_free_stmt($getResults);
+?>
 </table>
 </center>
 </body>
