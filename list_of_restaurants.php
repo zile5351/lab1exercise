1b81acfeceeafa64143b670495cfbe4846220006
zile5351 <60975884+zile5351@users.noreply.github.com>
2020-12-02 17:52:02 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -24,7 +24,7 @@
   <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:simplewebtp044739.database.windows.net,1433; Database = simpleweb", "simpleweb", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:simplewebtp044739.database.windows.net,1433; Database = simpleweb", "simpleweb", "Simple@web");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
