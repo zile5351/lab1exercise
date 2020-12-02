ac920b7ec95a181c28751d67e1402f1e6e7ea25e
zile5351 <60975884+zile5351@users.noreply.github.com>
2020-12-02 17:28:29 +0800

    Create list_of_restaurants.php

---
+++ list_of_restaurants.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>List of KL Restaurant</title>
+</head>
+<body>
+<center>
+<h1>List of KL Restaurant</h1>
+<!--create table structure using HTML first-->
+<table border="1">
+<tr>
+<th>Restaurant ID</th>
+<th>Restaurant Name</th>
+<th>Address</th>
+<th>Phone</th>
+</tr>
+<tr>
+<th>0</th>
+<th>Subway Restaurant</th>
+<th>Menara Standard Chartered, TPM</th>
+<th>03-22441234</th>
+</tr>
+</table>
+</center>
+</body>
+</html>
